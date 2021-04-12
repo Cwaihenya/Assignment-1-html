821f077826bb3e7f131b210df65afe1a469c7cc5
waihenya <cm.waihenya@gmail.com>
2021-04-12 08:17:51 +0000

    Initial commit

---
+++ discount.js
@@ -0,0 +1,4 @@
+const hasStudentCard = true; // Assign true to the constant
+if (hasStudentCard === true) { // If you bring your student ID
+  console.log("Student discount applied"); // "Student discount applied" is output to the console
+}
